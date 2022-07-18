22e6b05f0f973c46e1875fb7dde3ec8412528b8b
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 09:41:32 -0400

    Add polling for changes in the jenkins script

--- cicd.groovy
+++ cicd.groovy
@@ -1,5 +1,23 @@
 node('linux') 
 {
+        stage ('Poll') {
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/gettextport.git']]])
+                ])
+
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/utils.git']]])
+                ])
+        }
+
         stage('Build') {
                 build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'gettextport'), string(name: 'DESCRIPTION', 'gettext is an internationalization and localization system commonly used for writing multilingual programs on Unix-like computer operating systems.' )]
         }
