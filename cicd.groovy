e4047a4705cc40656fef368c7cb8bb98a42f4db0
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-19 21:21:28 -0400

    Remove polling of utils.git

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,6 @@ node('linux')
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'gettextport'), string(name: 'DESCRIPTION', 'gettext is an internationalization and localization system commonly used for writing multilingual programs on Unix-like computer operating systems.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'gettextport'), string(name: 'DESCRIPTION', value: 'gettext is an internationalization and localization system commonly used for writing multilingual programs on Unix-like computer operating systems.' )]
         }
 }
