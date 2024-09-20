5973991830afbb1c583b7c94f7cfb8ac10b3da24
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:31 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/gettextport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/gettextport.git']]])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/gettextport.git'), string(name: 'PORT_DESCRIPTION', value: 'gettext is an internationalization and localization system commonly used for writing multilingual programs on Unix-like computer operating systems.' ), string(name: 'NODE_LABEL', value: "v2r4") ]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/gettextport.git'), string(name: 'PORT_DESCRIPTION', value: 'gettext is an internationalization and localization system commonly used for writing multilingual programs on Unix-like computer operating systems.' ), string(name: 'NODE_LABEL', value: "v2r4") ]
         }
 }
